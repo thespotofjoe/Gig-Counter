52895e8c3f267996c3c68332fcde842bee2135c5
Joe Buchoff <joebuchoff@gmail.com>
2020-08-19 08:33:19 -0500

    Created Swift files for each screen, connected them to Storyboard, deleted old files.

---
+++ Gig Counter WatchKit Extension/CurrentEarningsScreen.swift
@@ -0,0 +1,9 @@
+//
+//  CurrentEarningsScreen.swift
+//  Gig Counter WatchKit Extension
+//
+//  Created by Joe on 8/19/20.
+//  Copyright © 2020 Joe's Studio. All rights reserved.
+//
+
+import Foundation
