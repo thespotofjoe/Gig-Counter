c7dd7915f2ade3a2ce7d6121b4bd5784ca3254ad
Joe <joebuchoff@gmail.com>
2020-09-04 16:38:26 -0500

    Removed old version. About to add new XCode Proj with updated code.

--- Gig Counter WatchKit Extension/CurrentEarningsScreen.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CurrentEarningsScreen.swift
-//  Gig Counter WatchKit Extension
-//
-//  Created by Joe on 8/19/20.
-//  Copyright © 2020 Joe's Studio. All rights reserved.
-//
-
-import Foundation
